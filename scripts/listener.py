3cd93b685125d7351363fe538ef569020057c44e
NgTatu <taituan.ng@gmail.com>
2021-03-17 21:10:09 +0700

    Create new branch
    Tuan

--- scripts/listener.py
+++ scripts/listener.py
@@ -11,12 +11,28 @@ def temp_callback(data):
 def humid_callback(data):
 	rospy.loginfo('Humidity %f', data.data)
 
+##### Test fuction, fix later #####
+def temp_actuator(data):
+    if data.data > 25:
+        rospy.loginfo('Hot!')
+    else:
+        rospy.loginfo('Cool')
+
+##### Test fuction, fix later #####
+def humid_actuator(data):
+    if data.data > 80:
+        rospy.loginfo('Too humid')
+    else:
+        rospy.loginfo('Kinda humid')
+
 def listener():
 
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber('/temperature', Float32, temp_callback)
     rospy.Subscriber('/humidity', Float32, humid_callback)
+    rospy.Subscriber('/temperature', Float32, temp_actuator)
+    rospy.Subscriber('/humidity', Float32, humid_actuator)
 
     rospy.spin()
 
